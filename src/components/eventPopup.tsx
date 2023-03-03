9167e48088ef66b7eaca30f536adb72bc5387864
adamdgit <adam_d@outlook.com>
2023-03-04 01:45:42 +1100

    renamed to selected date

--- src/components/eventPopup.tsx
+++ src/components/eventPopup.tsx
@@ -7,7 +7,7 @@ type eventPopupProps = {
 export default function eventPopup({ popupIsVisible, selectedDate } : eventPopupProps ) {
   return (
     <div className='popup' style={popupIsVisible ? {opacity: '1'} : {opacity: '0'}}>
-      <p>Date: {selectedDate}</p>
+      <p>Selected Date: {selectedDate}</p>
       <p>Add event description:</p>
       <input type="text" />
     </div>
