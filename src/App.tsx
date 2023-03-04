c50aa2563afa1131d8f38f268c768fd4fa6dc64b
adamdgit <adam_d@outlook.com>
2023-03-04 14:01:18 +1100

    Get saved events from local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,37 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import Calendar from "./components/Calendar"
 import EventPopup from "./components/eventPopup"
+import LocalStorageItems from "./components/LocalStorageItems"
+
+export type localStorageProps = {
+  'Date': string,
+  'Description': string
+}
 
 function App() {
 
-  const [popupIsVisible, setPopupIsVisible] = useState<Boolean>(false)
-  const [selectedDate, setSelectedDate] = useState<String>('')
+  const [popupIsVisible, setPopupIsVisible] = useState<boolean>(false)
+  const [selectedDate, setSelectedDate] = useState<Date>()
+  const [lsItems, setLsItems] = useState<localStorageProps[]>([])
 
+  // get events stored in LS on mount
+  useEffect(() => {
+    const LS_ITEMS = JSON.parse(localStorage.getItem('Calendar events'));
+    setLsItems(LS_ITEMS === null ? [] : LS_ITEMS);
+  },[])
+  
   return (
     <div className="App">
 
       <div className="left">
-
+        <h1>Your saved events:</h1>
+        <LocalStorageItems 
+          lsItems={lsItems} 
+        />
       </div>
 
       <div className="right">
-        <h1>Calendar event organiser</h1>
+        <h2>Calendar event organiser</h2>
         <Calendar 
           setPopupIsVisible={setPopupIsVisible} 
           setSelectedDate={setSelectedDate}
