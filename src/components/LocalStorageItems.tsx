3ca1e09e20217510710d5b11ec3e0ed4335ad2ac
adamdgit <adam_d@outlook.com>
2023-03-04 14:04:06 +1100

    Removed font awesome package

--- src/components/LocalStorageItems.tsx
+++ src/components/LocalStorageItems.tsx
@@ -1,4 +1,3 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome/"
 import { localStorageProps } from "../App"
 
 export default function LocalStorageItems({ lsItems }:localStorageProps[]) {
