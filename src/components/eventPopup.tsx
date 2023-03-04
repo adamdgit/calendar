330a15fba9d3ca2c5b0fc7942077d1f7ee335814
adamdgit <adam_d@outlook.com>
2023-03-04 14:02:48 +1100

    Save user entered event to local storage

--- src/components/eventPopup.tsx
+++ src/components/eventPopup.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react"
 
 type eventPopupProps = {
   popupIsVisible: boolean,
@@ -5,11 +6,26 @@ type eventPopupProps = {
 }
 
 export default function eventPopup({ popupIsVisible, selectedDate } : eventPopupProps ) {
+  
+  const [description, setDescription] = useState<String>('')
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    const LS_ITEMS = JSON.parse(localStorage.getItem('Calendar events')) || [];
+    let newStorage = { 
+      'Date': selectedDate,
+      'Description': description
+    };
+    LS_ITEMS.push(newStorage);
+    localStorage.setItem('Calendar events', JSON.stringify(LS_ITEMS));
+  };
+  
   return (
-    <div className='popup' style={popupIsVisible ? {opacity: '1'} : {opacity: '0'}}>
+    <form onSubmit={(e) => handleSubmit(e)} className='popup' style={popupIsVisible ? {opacity: '1'} : {opacity: '0'}}>
       <p>Selected Date: {selectedDate}</p>
       <p>Add event description:</p>
-      <input type="text" />
-    </div>
+      <input type="text" onChange={(e) => setDescription(e.target.value)} />
+      <button type="submit">Create reminder</button>
+    </form>
   )
-}
+};
