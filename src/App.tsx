e73b9bcc726a3a5e3af8e9818add4f3df50e657e
adamdgit <adam_d@outlook.com>
2023-03-08 00:47:06 +1100

    Fixed type errors

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,23 @@ import Calendar from "./components/Calendar"
 import LocalStorageItems from "./components/LocalStorageItems"
 
 export type localStorageProps = {
+  'id': number,
   'Date': string,
   'Description': string
 }
 
 function App() {
 
-  const [lsItems, setLsItems] = useState<localStorageProps[]>([])
+  const [lsItems, setLsItems] = useState<localStorageProps[] | []>([])
 
   // get events stored in LS on mount
   useEffect(() => {
-    const LS_ITEMS = JSON.parse(localStorage.getItem('Calendar events'));
+    const LS_ITEMS:localStorageProps[] = JSON.parse(localStorage.getItem('Calendar events'));
     setLsItems(LS_ITEMS === null ? [] : LS_ITEMS);
   },[])
   
   return (
     <div className="App">
-
       <div className="left">
         <h1>Your saved events:</h1>
         <LocalStorageItems 
@@ -28,10 +28,9 @@ function App() {
       </div>
 
       <div className="right">
-        <h2>Calendar event organiser</h2>
+        <h2>Your Calendar:</h2>
         <Calendar />
       </div>
-      
     </div>
   )
 }
