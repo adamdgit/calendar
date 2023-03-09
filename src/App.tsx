331683bbb298c7a01393461099ccc7f350a32780
adamdgit <adam_d@outlook.com>
2023-03-10 00:34:14 +1100

    Moved state back to app, needeed in children

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Calendar from "./components/Calendar"
 import EventPopup from "./components/eventPopup"
 import LocalStorageItems from "./components/LocalStorageItems"
+
+export type localStorageData = {
+  'Id': number,
+  'Date': string,
+  'Description': string
+}
 
 function App() {
 
   const [selectedDate, setSelectedDate] = useState<Date>();
   const [popupIsVisible, setPopupIsVisible] = useState<boolean>(false);
+  const [lsItems, setLsItems] = useState<localStorageData[] | []>([])
+
+  // get events stored in LS on mount
+  useEffect(() => {
+    const LS_ITEMS:localStorageData[] = JSON.parse(localStorage.getItem('Calendar events'));
+    setLsItems(LS_ITEMS === null ? [] : LS_ITEMS);
+  },[])
   
   return (
     <div className="App">
       <EventPopup 
         popupIsVisible={popupIsVisible} 
+        setPopupIsVisible={setPopupIsVisible}
         selectedDate={selectedDate}
+        setLsItems={setLsItems}
       />
 
       <div className="left">
         <h1>Your saved events:</h1>
-        <LocalStorageItems />
+        <LocalStorageItems 
+          setLsItems={setLsItems}
+          lsItems={lsItems}
+        />
       </div>
 
       <div className="right">
