3bf05484568799a69c8ac9a8f811ae5b3669ea1f
adamdgit <adam_d@outlook.com>
2023-03-09 23:44:44 +1100

    Moved storage state to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,13 @@
-import { useEffect, useState } from "react"
 import Calendar from "./components/Calendar"
 import LocalStorageItems from "./components/LocalStorageItems"
 
-export type localStorageProps = {
-  'id': number,
-  'Date': string,
-  'Description': string
-}
-
 function App() {
-
-  const [lsItems, setLsItems] = useState<localStorageProps[] | []>([])
-
-  // get events stored in LS on mount
-  useEffect(() => {
-    const LS_ITEMS:localStorageProps[] = JSON.parse(localStorage.getItem('Calendar events'));
-    setLsItems(LS_ITEMS === null ? [] : LS_ITEMS);
-  },[])
   
   return (
     <div className="App">
       <div className="left">
         <h1>Your saved events:</h1>
-        <LocalStorageItems 
-          lsItems={lsItems} 
-        />
+        <LocalStorageItems />
       </div>
 
       <div className="right">
