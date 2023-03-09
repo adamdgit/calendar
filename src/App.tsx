b102d30679402d1e9acf73bbaadb8b3ef212a537
adamdgit <adam_d@outlook.com>
2023-03-10 00:08:57 +1100

    Moved event popup out of each day option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Calendar from "./components/Calendar"
+import EventPopup from "./components/eventPopup"
 import LocalStorageItems from "./components/LocalStorageItems"
 
 function App() {
+
+  const [selectedDate, setSelectedDate] = useState<Date>();
+  const [popupIsVisible, setPopupIsVisible] = useState<boolean>(false);
   
   return (
     <div className="App">
+      <EventPopup 
+        popupIsVisible={popupIsVisible} 
+        selectedDate={selectedDate}
+      />
+
       <div className="left">
         <h1>Your saved events:</h1>
         <LocalStorageItems />
@@ -12,7 +22,10 @@ function App() {
 
       <div className="right">
         <h2>Your Calendar:</h2>
-        <Calendar />
+        <Calendar 
+          setPopupIsVisible={setPopupIsVisible}
+          setSelectedDate={setSelectedDate}
+        />
       </div>
     </div>
   )
