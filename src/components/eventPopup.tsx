457e27cb4be9119898a58625920c6e46a63d7187
adamdgit <adam_d@outlook.com>
2023-03-10 00:34:39 +1100

    Added types for LS_ITEMS

--- src/components/eventPopup.tsx
+++ src/components/eventPopup.tsx
@@ -1,28 +1,34 @@
 import { useState } from "react"
+import { localStorageData } from "../App";
 
 type eventPopupProps = {
   popupIsVisible: boolean,
-  selectedDate: Date
+  setPopupIsVisible: (args: boolean) => void,
+  selectedDate: Date,
+  setLsItems: (args: localStorageData[] | []) => void
 }
 
-export default function eventPopup({ popupIsVisible, selectedDate } : eventPopupProps ) {
+export default function eventPopup(
+  { popupIsVisible, setPopupIsVisible, selectedDate, setLsItems } : eventPopupProps ) {
   
-  const [description, setDescription] = useState<String>('')
+  const [description, setDescription] = useState<string>('')
 
   function createNewLsItem() {
-    const LS_ITEMS = JSON.parse(localStorage.getItem('Calendar events')) || [];
-    let newStorage = { 
+    const LS_ITEMS: localStorageData[] | [] = JSON.parse(localStorage.getItem('Calendar events')) || [];
+    let newStorage: localStorageData = { 
       'Id': LS_ITEMS.length !== 0 ? LS_ITEMS[LS_ITEMS.length -1].Id +1 : 1,
-      'Date': selectedDate,
+      'Date': selectedDate.toString(),
       'Description': description
     };
     LS_ITEMS.push(newStorage);
     localStorage.setItem('Calendar events', JSON.stringify(LS_ITEMS));
+    setLsItems(LS_ITEMS);
+    setPopupIsVisible(false);
   };
   
   return (
     <div className='popup' style={popupIsVisible ? {opacity: '1', pointerEvents: 'all'} : {opacity: '0', pointerEvents: 'none'}}>
-      <p>Selected: {selectedDate}</p>
+      <span className="heading">Selected: {selectedDate}</span>
       <p>Add event info:</p>
       <input type="text" onChange={(e) => setDescription(e.target.value)} />
       <button onClick={() => createNewLsItem()}>Create reminder</button>
