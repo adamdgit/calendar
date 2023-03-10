09500f4633fd38cd16a9b6983701ff1043bf6091
adamdgit <adam_d@outlook.com>
2023-03-10 17:24:24 +1100

    Removed highlight for selected day

--- src/components/DayOption.tsx
+++ src/components/DayOption.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 type dayOptionProps = {
   day: Date,
@@ -11,12 +10,9 @@ export default function DayOption(
   { day, monthSelect, setPopupIsVisible, setSelectedDate } 
   : dayOptionProps) {
 
-  const [isSelected, setIsSlected] = useState<boolean>(false)
-
   function handlePopup(e: any) {
     console.log(e.target)
     setPopupIsVisible(true);
-    setIsSlected(true);
     // pass slected date value to popup
     setSelectedDate(e.target.value)
   }
@@ -24,7 +20,6 @@ export default function DayOption(
   return (
     <div style={{position: 'relative'}}>
       <button
-        style={isSelected ? {background: '#74b5e3', color: 'black'} : {background: ''}}
         onClick={(e) => handlePopup(e)}
         className={
           day.toLocaleString('en-us', { day: '2-digit', month: '2-digit', year: 'numeric' }) === new Date().toLocaleString('en-us', { day: '2-digit', month: '2-digit', year: 'numeric' }) ? 'date today' 
